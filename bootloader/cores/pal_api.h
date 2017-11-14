2cc9c7b7620298070152977228a50b5f8001dd47
Bob Cousins <bobcousins42@googlemail.com>
2017-11-14 18:30:13 +0000

    Update makefile

---
+++ bootloader/cores/pal_api.h
@@ -0,0 +1,57 @@
+/**************************************************************************
+ Copyright (c) 2017 Bob Cousins bobcousins42@googlemail.com
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+****************************************************************************/
+
+// **************************************************************************
+// Description:
+//
+// **************************************************************************
+
+#ifndef _PAL_API_H
+#define _PAL_API_H
+
+// --------------------------------------------------------------------------
+// Includes
+// --------------------------------------------------------------------------
+
+#include "pal_ios.h"
+#include "pal_target.h"
+#include "pal_flash_iap.h"
+#include "pal_uart.h"
+
+// --------------------------------------------------------------------------
+// Defines
+// --------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------
+// Types
+// --------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------
+// Public Variables
+// --------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------
+// Public functions
+// --------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------------
+//
+// --------------------------------------------------------------------------
+
+#endif // _PAL_API_H
+
